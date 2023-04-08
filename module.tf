6570b6bb01d1c481277e3c3353368ee509d9e22a
lucas-020 <projectx7cluster@gmail.com>
2023-04-08 05:46:45 +0000

    eks

---
+++ module.tf
@@ -0,0 +1,109 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "my-cluster"
+  cluster_version = "1.24"
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets #for worker nodes
+
+  enable_irsa = true # enables iam roles for service accounts
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  # vpc_id                   = "vpc-0dedf2a083bd7fc74"
+  # subnet_ids               = ["subnet-0138bbc1c8d473984", "subnet-04ca4a966946c9926", "subnet-01e264a2e2d051275"]
+#   control_plane_subnet_ids = ["subnet-0d174d04d0a598645", "subnet-0a1da554db49d4b9e", "subnet-082d49ea94c2cec46"]
+
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "m6i.large"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = {
+      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
+  }
+
+  self_managed_node_groups = {
+    one = {
+      name         = "mixed-1"
+      max_size     = 5
+      desired_size = 2
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "m5.large"
+            weighted_capacity = "1"
+          }
+        ]
+      }
+    }
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m5.large",]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+#   # aws-auth configmap
+#   manage_aws_auth_configmap = true
+
+#   aws_auth_roles = [
+#     {
+#       rolearn  = "arn:aws:iam::66666666666:role/role1"
+#       username = "role1"
+#       groups   = ["system:masters"]
+#     },
+#   ]
+
+#   aws_auth_users = [
+#     {
+#       userarn  = "arn:aws:iam::66666666666:user/user1"
+#       username = "user1"
+#       groups   = ["system:masters"]
+#     },
+#     {
+#       userarn  = "arn:aws:iam::66666666666:user/user2"
+#       username = "user2"
+#       groups   = ["system:masters"]
+#     },
+#   ]
+# }
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+}
+
+	git config --global  user.name     lucas-020
+  git config --global  user.email    projectx7cluster@gmail.com
