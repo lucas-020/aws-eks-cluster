5b1ba2b33f532dd43907b06d02aeeaebe972f8d7
lucas-020 <projectx7cluster@gmail.com>
2023-04-13 01:38:56 +0000

    deploy kubernetes

--- localscript.sh
+++ localscript.sh
@@ -6,4 +6,8 @@ sudo ln -s ~/.tfenv/bin/* /usr/local/bin
 tfenv install 1.1.1
 tfenv use 1.1.1
 
-# bash localscript.sh
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bash_profile
